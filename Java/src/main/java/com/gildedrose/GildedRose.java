47ed6510b0479d5963df87c0b2d177904d3d4689
akafortune <hcm092002@gmail.com>
2026-02-28 22:28:45 -0500

    Refactor

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -8,82 +8,64 @@ class GildedRose {
     }
 
     public void updateQuality() {
+    	int upperQualityThreshold = 50;
+    	int lowerQualityThreshold = 0;
+    	int lowerSellInThreshold = 0;
+    	
         //loop through all items
         for (int i = 0; i < items.length; i++) {
-            //if the item is not named aged brie and is not named backstage passes
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                //and if it's quality is greater than 0
-                if (items[i].quality > 0) {
-                    //AND if its name isn't sulfuras, hand of ragnaros
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        //decrease it's quality
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            //else if the item is named either aged brie or backstage passes
-            } else {
-                //and if it's quality is less than 50
-                if (items[i].quality < 50) {
-                    //increase the item's quality by one
-                    items[i].quality = items[i].quality + 1;
+        	
+        	int quality = items[i].quality;
+        	int sellIn = items[i].sellIn;
+        	
+        	
+        	if(items[i].name.equals("Aged Brie")){
+        		
+        		if (quality < upperQualityThreshold) {
+        			quality++;
+        		}
+        		
+        		sellIn--;
+        		
+        		if(sellIn < lowerSellInThreshold && quality < upperQualityThreshold) {
+                    quality++;
+        		}
+        	} else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
 
-                    //and if the item's name is backstage
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        //and the sellIn value is less than 11
-                        if (items[i].sellIn < 11) {
-                            //AND the quality is less than 50
-                            if (items[i].quality < 50) {
-                                //increase the quality by 1 again
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                        //and the sell in value is less than 6
-                        if (items[i].sellIn < 6) {
-                            //AND the quality is less than 50
-                            if (items[i].quality < 50) {
-                                //increase quality by 1
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
+        		if (quality < upperQualityThreshold) {
+                    quality++;
+                    
+                    if(sellIn < 11 && sellIn >= 6) {
+                    	quality++;
+                    } else if(sellIn < 6) {
+                    	quality += 2;
                     }
+                    
+                    if(quality > upperQualityThreshold) {
+                    	quality = upperQualityThreshold;
+                    }
+        		}
+        		
+                sellIn--;
+                
+                if(sellIn < lowerSellInThreshold) {
+                	quality = 0;
                 }
-            }
-
-            //if the name isnt sulfuras
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                //decrease sell in by 1
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            //if the item's sell in is less than 0
-            if (items[i].sellIn < 0) {
-                //and if the name is not aged brie
-                if (!items[i].name.equals("Aged Brie")) {
-                    // AND its name isnt backstage passes
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        //AND it's quality is less than 0
-                        if (items[i].quality > 0) {
-                            //AND its name isn't sulfuras hand of ragnaros
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                //decrease quality by 1
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    //if the name IS backstage passes
-                    } else {
-                        //set quality to 0 (WTF)
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                // if its name is aged brie
-                } else {
-                    //and the quality is less than 50
-                    if (items[i].quality < 50) {
-                        //increase quality by 1
-                        items[i].quality = items[i].quality + 1;
-                    }
+        		
+        	} else if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+        		if (quality > lowerQualityThreshold) {
+                    quality--;
                 }
-            }
+        		
+        		sellIn--;
+        		
+        		if(sellIn < lowerSellInThreshold && quality > lowerQualityThreshold) {
+        			quality--;
+        		}
+        	} 
+        	
+        	items[i].quality = quality;
+        	items[i].sellIn = sellIn;
         }
     }
 }
